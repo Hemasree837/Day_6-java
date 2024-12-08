bde62a9c9753a3b953ad06462d7adaf24020af23
Hemasree837 <hemasree837@gmail.com>
2024-12-08 08:15:31 +0530

    Add files via upload

---
+++ AcceptingString.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class AcceptingString{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        String arr[]=new String[a];
+        for(int i=0;i<a;i++){
+            arr[i]=sc.next();
+        }
+        for(int i=0;i<a;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+/*
+*5
+krishna 
+radha
+rama
+seetha
+vishnu
+krishna radha rama seetha vishnu
+*/
